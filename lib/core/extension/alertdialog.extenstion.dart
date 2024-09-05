d30748e970f37737916d83350171f187092a4032
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-09-05 19:13:10 +0100

    feat: Add extension for showing AlertDialog
    This commit adds a new extension `AlertDialogExtension` on `BuildContext` to provide a convenient way to show an `AlertDialog` with customizable options. The extension includes methods for displaying a dialog box with a title, body, content widget, confirm and cancel buttons, and custom callbacks. Additionally, it introduces a `showPopUp` method for showing a pop-up with a specified content widget. This extension enhances the reusability and readability of code that requires showing alert dialogs or pop-ups, improving the overall user experience.
    
    Related to recent user commits:
    - Integrate router and ScreenUtil support
    - feat: Add FlutterSecureStorage plugin registration for Linux and Windows
    - feat: Add ApiConstants class for managing API base URLs
    - feat: Refactor main app structure for production
    - feat: Add `DocQuizAPP` class for initializing the Flutter application
    - feat: Add dependency injection container setup
    - feat: Add `flutter_screenutil` and `flutter_secure_storage` dependencies
    - Added `ApiResult` Freezed Models
    - Introduced ApiResult and RepoResult types
    - Added JSON serialization for API response models
    - Added API response models
    - feat: Add TryCallApi class for handling API calls with error handling
    - chore: Add ApiErrorHandler class for handling Dio exceptions
    - Add Dio networking setup with interceptors
    - Added development and production flavors
    - Updated dependencies for enhanced functionality

---
+++ lib/core/extension/alertdialog.extenstion.dart
@@ -0,0 +1,66 @@
+import 'package:app/core/extension/navigator.extension.dart';
+import 'package:flutter/material.dart';
+
+extension AlertDialogExtension on BuildContext {
+  Future<T?> showDialogBox<T>({
+    required String title,
+    String body = '',
+    Widget? content,
+    String? confirmText,
+    void Function(VoidCallback)? onConfirm,
+    String? cancelText,
+    void Function(VoidCallback)? onCancel,
+    bool canPop = true,
+  }) async {
+    return showDialog<T>(
+      context: this,
+      builder: (context) {
+        return PopScope(
+          canPop: canPop,
+          child: AlertDialog(
+            title: Text(title),
+            content: content ?? Text(body),
+            actions: <Widget>[
+              if (cancelText != null)
+                TextButton(
+                  onPressed: () {
+                    if (onCancel != null) onCancel(context.back);
+                  },
+                  child: Text(
+                    cancelText,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                ),
+              if (confirmText != null)
+                TextButton(
+                  onPressed: () {
+                    if (onConfirm != null) onConfirm(context.back);
+                  },
+                  child: Text(
+                    confirmText,
+                    style: const TextStyle(color: Colors.blue),
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<T?> showPopUp<T>({
+    required Widget content,
+  }) async {
+    return showDialog<T>(
+      context: this,
+      builder: (context) {
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            content,
+          ],
+        );
+      },
+    );
+  }
+}
