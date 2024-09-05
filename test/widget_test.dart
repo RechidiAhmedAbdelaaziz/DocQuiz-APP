871067faeb0d6c431b84babb3e33ab2d70f8be6b
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-09-05 19:02:54 +0100

    feat: Add FlutterSecureStorage plugin registration for Linux and Windows
    This commit adds the registration of the `FlutterSecureStorage` plugin for Linux and Windows platforms. The plugin is registered in the `generated_plugin_registrant.cc` file for Linux and in the `generated_plugin_registrant.cc` file for Windows. This addition enables secure storage functionality in the Flutter application on these platforms, enhancing the overall security and data protection.
    
    Related to recent user commits:
    - feat: Add `flutter_secure_storage` dependency
    - feat: Add `flutter_secure_storage` dependency for Windows
    - feat: Add `flutter_secure_storage` dependency for Linux

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/main.dart';
+import 'package:app/app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DocQuizAPP());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
