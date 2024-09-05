9dff12519214dcc2f9496562bed8a4d0c21b5aeb
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-09-05 18:49:00 +0100

    feat: Add TryCallApi class for handling API calls with error handling
    This commit adds a new class called TryCallApi, which provides a convenient way to make API calls with error handling. The class includes a static method called `call` that takes a function representing the API call and returns a `Future<ApiResult<T>>`. Inside the method, the API call is wrapped in a try-catch block to handle any errors that may occur. If an error is caught, it is logged using `debugPrint` and converted into an `ApiResult.failure` using the `ApiErrorHandler` class. This class will be useful for handling API calls throughout the project, ensuring consistent error handling and improving the overall reliability of the app.

---
+++ lib/core/network/try_call_api.dart
@@ -0,0 +1,18 @@
+
+import 'package:app/core/types/api_result.type.dart';
+import 'package:flutter/material.dart';
+
+import 'api_error.handler.dart';
+
+class TryCallApi {
+  static Future<ApiResult<T>> call<T>(
+      Future<T> Function() apiCall) async {
+    try {
+      return ApiResult.success(await apiCall());
+    } catch (error) {
+      debugPrint(
+          '\n\n!!!\n TryCallApi error: ${error.toString()} \n!!!\n\n');
+      return ApiResult.failure(ApiErrorHandler.handle(error));
+    }
+  }
+}
