67025dce28d26ff57900029b7b671dcbeb52afd1
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-09-05 19:02:32 +0100

    feat: Add ApiConstants class for managing API base URLs
    This commit introduces the `ApiConstants` class, which provides constants for the development and production API base URLs. By centralizing these values, we improve code maintainability and facilitate easier switching between different environments. This addition enhances the modularity and scalability of our application, aligning with best practices for managing network-related constants.
    
    Related to recent user commits:
    - feat: Refactor main app structure for production
    - feat: Add `DocQuizAPP` class for initializing the Flutter application
    - feat: Add dependency injection container setup
    - feat: Add `flutter_screenutil` and `flutter_secure_storage` dependencies
    - Added `ApiResult` Freezed Models
    - Introduced ApiResult and RepoResult types
    - Added JSON serialization for API response models
    - Added API response models
    - feat: Add TryCallApi class for handling API calls with error handling
    - chore: Add ApiErrorHandler class for handling Dio exceptions
    - Add Dio networking setup with interceptors
    - Added development and production flavors
    - Updated dependencies for enhanced functionality

---
+++ lib/core/network/api.constants.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+class ApiConstiants {
+  static const String DEV_BASE_URL = 'http://localhost:3000/';
+  static const String PROD_BASE_URL = 'https://api.docquizz.top/';
+}
